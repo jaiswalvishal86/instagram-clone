1fc2bb350b18b642ad9802e1cc738ad0ccd50806
Vishal Jaiswal <jaiswalvishal86@gmail.com>
2020-04-15 15:30:41 +0530

    Created Prisma database as well as Express server and created account for users, implemented request secret key for users

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# instagram-clone
+# Instagram-Clone-App
+
 Instagram Clone App using React + React-Native + Express + Prisma + Apollo + Graphql
+
+## User Stories
+
+- [x] Create Account
+- [x] Requset Secret
+- [ ] Confirm Secret (Login)
+- [ ] Like/Unlike a photo
+- [ ] Comment on a photo
+- [ ] Search by user
+- [ ] Search by location
+- [ ] See user profile
+- [ ] Follow/Unfollow Users
+- [ ] See Full Photo
+- [ ] Edit my profie
+- [ ] Login
+- [ ] Edit a photo(Delete)
+- [ ] See the feed
---
+++ src/utils.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+import { adjectives, nouns } from './words';
+import nodemailer from 'nodemailer';
+import sgTransport from 'nodemailer-sendgrid-transport';
+
+export const generateSecret = () => {
+	const randomNumber = Math.floor(Math.random() * adjectives.length);
+	return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = (email) => {
+	const options = {
+		auth: {
+			api_user: process.env.SENDGRID_USERNAME,
+			api_key: process.env.SENDGRID_PASSWORD
+		}
+	};
+	const client = nodemailer.createTransport(sgTransport(options));
+	return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+	const email = {
+		from: 'jaiswalvishal86@gmail.com',
+		to: address,
+		subject: 'Login Secret for Instagram-Clone🔐',
+		html: `<p>Hello User! Your login secret 🔑 is <b>${secret}</b>.<br/>Copy and paste it to the Instagram-Clone Application/Website to login🙂</p>`
+	};
+	return sendMail(email);
+};
